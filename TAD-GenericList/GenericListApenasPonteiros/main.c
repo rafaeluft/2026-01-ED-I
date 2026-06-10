6aae171b683bd57ef962c1be5577bce3039cf0fb
pprafaellima <pp.rafaellima@gmail.com>
2026-06-10 09:28:52 -0300

    Tentativa de lista genérica com ponteiros

--- TAD-GenericList/GenericListApenasPonteiros/main.c
+++ TAD-GenericList/GenericListApenasPonteiros/main.c
@@ -1,24 +1,52 @@
 #include "TGList.h"
 #include <stdio.h>
+
+
+void print_float(void* pinfo){
+    float *p = (float*) pinfo;
+    printf("(%0.2f)-> ", *p);
+}
+
 void print_int(void* pinfo){
     int *p = (int*) pinfo;
-    printf("%d -> ", *p);
+    printf("(%d)-> ", *p);
+}
+
+void print_s(void* pinfo){
+    char* ps = (char*) pinfo;
+    printf("%s ", ps);
 }
 int main(){
     //A lista1 só insere no início
-    TGList* lista1 = TGList_create(NO_INICIO, print_int);
+    TGList* lista1 = TGList_create(NO_INICIO, print_float);
     //A lista2 só insere no fim
-    TGList* lista2 = TGList_create(NO_FIM, print_int);
-    int V[] = {1, 2, 3, 4, 5}, i;
-    for(i = 0; i<5; i++){
+    TGList* lista2 = TGList_create(NO_FIM, print_float);
+    //int V[] = {1, 2, 3, 4, 5}, i;
+    float V[] = {1.3, 2.2, 3.45, 4.678, 5.7778};
+    for(int i = 0; i<5; i++){
         if(!TGList_insert(lista1, &V[i]))
-            printf("Ocorreu um erro ao inserir o V[%i]=%i\n", i, V[i]);
+            printf("Ocorreu um erro ao inserir o V[%i]=%f\n", i, V[i]);
         if(!TGList_insert(lista2, &V[i]))
-            printf("Ocorreu um erro ao inserir o V[%i]=%i\n", i, V[i]);
+            printf("Ocorreu um erro ao inserir o V[%i]=%f\n", i, V[i]);
     }
     puts("Lista 1 (politica de insercao no inicio):");
     TGList_print(lista1);
     puts("Lista 2 (politica de insercao no fim):");
     TGList_print(lista2);
+
+    //Problema: 
+    V[0] = 0.0;
+    puts("Lista 1 (politica de insercao no inicio):");
+    TGList_print(lista1);
+    puts("Lista 2 (politica de insercao no fim):");
+    TGList_print(lista2);
+
+    TGList* lista3 = TGList_create(NO_FIM, print_s);
+    char strings[3][64] = {"ED-I", "eh facil", "demais!"}, i;
+    for(i = 0; i<3; i++)
+        if(!TGList_insert(lista3, strings[i]))
+            printf("Ocorreu um erro ao inserir o V[%i]=%s\n", i, strings[i]);
+    puts("Lista de strings:");
+    TGList_print(lista3);
     return 0;
 }
